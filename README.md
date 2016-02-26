00448ea03dbbdd5f7109be3646dd57fffb08e7bd
= <=>
2016-02-26 03:02:11 -0300

    Versão 0.1.1 - Updates

---
+++ README.md
@@ -0,0 +1,25 @@
+## Sinopse
+
+Esta aplicação procura por Tweets que contenham as palavras pesquisas e mostra, de uma forma limpa, os dados: nome do usuário, horário do tweet, thumbnail e mensagem postada.
+É possível também parar o envio do Streaming!
+As ferramentas utilizadas, são:
+- NPM (Pacotes do nodeJS)
+- ExpressJS (Configuração do Server)
+- Socket.io (Comunicação entre Client e Server)
+- Twit (faz a comunicação com a API do Twitter)
+- Bootstrap 3
+- jQuery
+
+## Installation
+
+1. Faça um clone do Projeto
+2. Instale os pacotes do Node
+`$ npm install`
+3. Execute
+`$ node server.js`
+4. Abra seu navegador e acesse `localhost:8000`
+5. Divirta-se!
+
+## License
+
+The MIT License (MIT) - Copyright (c) 2016 emanuellarini
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 var express = require('express'),  
     app     = express(),
-    port    = process.env.PORT || 8068,
+    port    = process.env.PORT || 8000,
     server  = app.listen(port),
     io      = require('socket.io').listen(server);
 
@@ -10,10 +10,10 @@ var Twit = require('twit');
 
 var T = new Twit(  
 {
-	consumer_key:         'PIWIBShEWqWN2fcnoVaVQqNEE',
-	consumer_secret:      'i5d8caSKBNGW03AYyr4AhqXT4LA6NjrFBigG1mIbcyXXWgkuhq',
-	access_token:         '1421818958-Z9FXZcbfx2diP0fILC2AWXupUknn287W3g2MMfs',
-	access_token_secret:  'eXZqnKqXGGOlsZLdqul62snVIh5gKEHn0KwIvmqk1V5Gc'
+	consumer_key:         '',
+	consumer_secret:      '',
+	access_token:         '',
+	access_token_secret:  ''
 });
 
 app.use(express.static(__dirname + '/public'));
